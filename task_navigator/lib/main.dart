962b376d0b66e676b75292f453b7fb35adaf721c
dennis <info.dennis.durmus@gmail.com>
2024-10-02 13:19:57 +0200

    Screen One

--- task_navigator/lib/main.dart
+++ task_navigator/lib/main.dart
@@ -11,20 +11,26 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        bottomNavigationBar: NavigationBar(destinations: const [
+          NavigationDestination(icon: Icon(Icons.newspaper), label: "News"),
+          NavigationDestination(icon: Icon(Icons.heart_broken), label: "Likes"),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profil")
+        ]),
         appBar: AppBar(
           title: const Text("MyApp"),
           backgroundColor: Colors.lime,
         ),
-        body: const Column(
-          children: [Text(
-            'News', 
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-          ]
-            ),
-          ),
-          ]
+        body: const Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "News",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.newspaper),
+              ]),
         ),
       ),
     );
